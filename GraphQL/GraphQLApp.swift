e885bf4fc2c5ed1e8566eba7a36bda12c6e2bbc6
robh <rohan.bhale2411@gmail.com>
2023-02-02 02:49:55 +0530

    Initial Commit

---
+++ GraphQL/GraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLApp.swift
+//  GraphQL
+//
+//  Created by Rohan Bhale on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
